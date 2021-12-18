b735e3a5d79d8d22160fcd918e34b5c85efcc08a
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-12-18 13:45:03 -0500

    basic qutebrowser

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,25 @@
+
+config.load_autoconfig(False)
+
+# bindings
+config.bind('xb', 'config-cycle tabs.show always never')
+
+# colors
+# c.colors.tabs.bar.bg = ""
+
+# darkmode
+c.colors.webpage.darkmode.enabled       = True
+c.colors.webpage.darkmode.policy.images = "never"
+
+# tab bar
+c.tabs.last_close   = "close"
+c.tabs.title.format = "{private}{audio}{index}: {current_title}"
+
+# status bar
+c.statusbar.widgets = ["keypress", "progress", "url", "scroll", "history", "tabs", "text:qutebrowser"]
+
+# downloads
+c.downloads.location.directory = "~/Downloads"
+c.downloads.location.prompt    = True
+c.downloads.location.remember  = True
+
