9e1aca4fdd9710c93e5f1d56585a516b56e79a0f
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-07-26 00:25:13 -0400

    tachibana profile

--- files/xmonad/.config/xmonad/xmobar.hs
+++ files/xmonad/.config/xmonad/xmobar.hs
@@ -6,13 +6,18 @@ config = defaultConfig {
     font = "xft:Source Code Pro:size=12"
     , additionalFonts = []
     , position = Top
+    , bgColor = "#272822"
+    -- , borderColor = "#75715e"
+    -- , borderWidth = 1
+    -- , border = FullB
     , commands = [
-        Run $ Memory ["-t", "<used>M"] 10
-        , Run $ DiskU [("sda3", "<used>")] [] 20
-        , Run $ BatteryP ["BAT0"] ["-t", "<left>%"] 20
-        , Run $ Date "%a %b %_d %H:%M" "date" 10
+        Run $ Memory ["-t", "<fc=#f8f8f2,#f92672> <used>M </fc>"] 10
+        , Run $ DiskU [("sda3", "<fc=#272822,#f4bf75> <used> </fc>")] [] 20
+        , Run $ BatteryP ["BAT0"] ["-t", "<fc=#272822,#66d9ef> <left>%[<leftbar>] </fc>", "-b", " "] 20
+        , Run $ Date "<fc=#272822,#a6e22e> %a %b %d %H:%M </fc>" "date" 10
+        , Run $ XMonadLog
     ]
-    , template = "}{ %memory% | %disku% | %batteryp% | %date%"
+    , template = " %XMonadLog% }{ %memory% %disku% %battery% %date% <fc=#272822,#f8f8f2> [λ] </fc> "
 }
 
 main :: IO ()
--- files/xmonad/.config/xmonad/xmonad.hs
+++ files/xmonad/.config/xmonad/xmonad.hs
@@ -2,20 +2,26 @@
 import Data.Maybe
 import System.IO
 
-import XMonad
+import XMonad hiding ( (|||) )
 import XMonad.Util.EZConfig
 import XMonad.Layout.ThreeColumns
 
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
+
+-- import XMonad.Layout.LayoutCombinators
 
 -- utils
 -- readXRDB :: String -> IO String
 -- readXRDB key = fromMaybe "" . findValue key <$> runProcessWithoutInput "xrdb" ["-query"] ""
 
 main :: IO ()
-main = xmonad $ xmobarProp $ myConfg
+main = do
+    xmproc <- spawnPipe "xmobar .config/xmonad/xmobar.hs"
+    xmonad $ xmobarProp $ myConfg
 
 myConfg = def
     { modMask            = mod1Mask
@@ -24,6 +30,9 @@ myConfg = def
     , normalBorderColor  = "#75715e"
     , focusedBorderColor = "#a6e22e"
     }
+    -- `additionalKeysP`
+    -- [ ("M-f", sendMessage $ JumpToLayout "Full")
+    -- ]
 
 myLayout = tiled ||| threeCol ||| Full
     where
